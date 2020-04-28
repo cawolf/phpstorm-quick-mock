3327b0562f8abfc4d9243c1ecd3861130829a292
Christian Alexander Wolf <mail@cawolf.de>
2020-04-28 11:13:14 +0200

    fix: update dependencies and removed deprecations

--- src/main/kotlin/de/cawolf/quickmock/intention/service/ReformatTestcase.kt
+++ src/main/kotlin/de/cawolf/quickmock/intention/service/ReformatTestcase.kt
@@ -9,9 +9,9 @@ import com.jetbrains.php.lang.psi.elements.PhpClass
 
 class ReformatTestcase {
     fun invoke(project: Project, psiElement: PsiElement, clazz: PhpClass) {
-        val pdm = project.getComponent(PsiDocumentManager::class.java)
-        val document = pdm.getDocument(psiElement.containingFile)
-        pdm.doPostponedOperationsAndUnblockDocument(document!!)
+        val psiDocumentManager = ServiceManager.getService(project, PsiDocumentManager::class.java)
+        val document = psiDocumentManager.getDocument(psiElement.containingFile)
+        psiDocumentManager.doPostponedOperationsAndUnblockDocument(document!!)
 
         val csm = ServiceManager.getService(project, CodeStyleManager::class.java)
         csm.reformat(clazz, true)
