aa055074033ed91b10b903943031be3adec06a49
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-12-03 21:46:38 +0100

    chore: fix deprecations (#38)
    fix deprecations and replace travis with github workflows

--- src/main/kotlin/de/cawolf/quickmock/intention/service/AddMissingUseStatements.kt
+++ src/main/kotlin/de/cawolf/quickmock/intention/service/AddMissingUseStatements.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.jetbrains.php.codeInsight.PhpCodeInsightUtil
+import com.jetbrains.php.lang.psi.PhpGroupUseElement.PhpUseKeyword
 import com.jetbrains.php.refactoring.PhpAliasImporter
 import de.cawolf.quickmock.intention.PRIMITIVES
 
@@ -14,7 +15,7 @@ class AddMissingUseStatements {
         if (PRIMITIVES.contains(foldDocBlockTypeHintedArray.invoke(fqcn))) return false
 
         val scopeHolder = PhpCodeInsightUtil.findScopeForUseOperator(namespace)
-        if (scopeHolder != null && PhpCodeInsightUtil.alreadyImported(scopeHolder, fqcn) == null && PhpCodeInsightUtil.canImport(scopeHolder, fqcn)) {
+        if (scopeHolder != null && PhpCodeInsightUtil.findImportedName(scopeHolder, fqcn, PhpUseKeyword.CLASS) == null && PhpCodeInsightUtil.canImport(scopeHolder, fqcn, PhpUseKeyword.CLASS)) {
             PhpAliasImporter.insertUseStatement(fqcn, alias, scopeHolder)
         }
         return true
